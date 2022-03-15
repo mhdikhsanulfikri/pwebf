28843613f37621eaedbf42f8b3bf84b0d85dcee5
mhdikhsanulfikri <101613492+mhdikhsanulfikri@users.noreply.github.com>
2022-03-15 11:13:39 +0700

    pwebf

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title>This is a title</title>
+            <head>
+                <body>
+                    <h1>Heading 1</h1>
+                    <h2>Heading 1</h2>
+                    <h3>Heading 1</h3>
+                    <h4>Heading 1</h4>
+                    <h5>Heading 1</h5>
+                    <h6>Heading 1</h6>
+                    <p>Hello world!</p>
+                    <pre>Hello world!</pre>
+                        <p>Hello&nbsp;&nbsp;&nbsp;world!</p>
+                    <p>
+                        <b>bold</b>
+                        <strong>strong</strong>
+                        <i>italic</i>
+                        <em>emphasis</em>
+                        <u>underline</u>
+                        H<sub>2</sub>o
+                        x<sup>2</sup>
+                        <a href="profile.php"><Link></a>
+                        <a href="http://localhost/profile.php"><Link></a>
+                        <br><br><br><br><br><br><br><br><br><br>
+                        <a id="#tips">jump to Section A</a>
+                        <img src="images/batman-png.png">
+
+
+                    </p>
+</body>
+<html>
+                
